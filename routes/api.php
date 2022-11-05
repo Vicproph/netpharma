96103bf60db48271f7c8408d194214e28f87db65
Mohammad Arabzadeh <77565128+Vicproph@users.noreply.github.com>
2022-11-05 21:19:05 +0330

    Feature: add routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::resource('users', UserController::class);
+Route::post('users/login', UserController::class . '@login');
+Route::middleware('auth:api')->post('users/logout', UserController::class . '@logout');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:api')->resource('posts', PostController::class);
